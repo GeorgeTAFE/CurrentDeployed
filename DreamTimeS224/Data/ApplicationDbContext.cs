4396d3b12b2928a771f854f6255ed3f924d45a77
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2024-11-06 16:37:45 +1100

    Added seed data for Rooms and RoomTypes.

--- DreamTimeS224/Data/ApplicationDbContext.cs
+++ DreamTimeS224/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using DreamTimeS224.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace DreamTimeS224.Data
 {
@@ -23,7 +24,25 @@ namespace DreamTimeS224.Data
         {
             // In here, we can add custom configuration for our entities
 
+            // Auto-increment
+            builder.Entity<RoomType>(e => e.Property(e => e.Id).ValueGeneratedOnAdd());
 
+            // Add some seed data
+            builder.Entity<RoomType>().HasData(
+                new RoomType { Id = 1, Name = "Arcadia", Capacity = 130 },
+                new RoomType { Id = 2, Name = "Monstrocity", Capacity = 3 },
+                new RoomType { Id = 3, Name = "Book Worms", Capacity = 30}
+            );
+
+            builder.Entity<Room>().HasData(
+                new Room { Code = "A01", RoomTypeId = 1 },
+                new Room { Code = "A02", RoomTypeId = 1 },
+                new Room { Code = "A03", RoomTypeId = 1 },
+                new Room { Code = "M01", RoomTypeId = 2 },
+                new Room { Code = "M02", RoomTypeId = 2 },
+                new Room { Code = "M03", RoomTypeId = 2 },
+                new Room { Code = "M04", RoomTypeId = 2 }
+            );
 
             // Define foreign key cascade rules
             builder.Entity<Room>()
---
+++ DreamTimeS224/Data/Migrations/20241106053409_AddSeedDataForRoomAndRoomType.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DreamTimeS224.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedDataForRoomAndRoomType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "RoomTypes",
+                columns: new[] { "Id", "Capacity", "Name" },
+                values: new object[,]
+                {
+                    { 1, 130, "Arcadia" },
+                    { 2, 3, "Monstrocity" },
+                    { 3, 30, "Book Worms" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Code", "RoomTypeId" },
+                values: new object[,]
+                {
+                    { "A01", 1 },
+                    { "A02", 1 },
+                    { "A03", 1 },
+                    { "M01", 2 },
+                    { "M02", 2 },
+                    { "M03", 2 },
+                    { "M04", 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "A01");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "A02");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "A03");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "M01");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "M02");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "M03");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Code",
+                keyValue: "M04");
+
+            migrationBuilder.DeleteData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RoomTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
